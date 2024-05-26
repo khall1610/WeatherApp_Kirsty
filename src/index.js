da2bc21aa0dc00e00e12fb5c428358a09ab210ca
khall1610 <156712416+khall1610@users.noreply.github.com>
2024-05-26 18:39:40 +0100

    Updated CSS Styling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,52 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let currentDayElement = document.querySelector("#day");
+  let currentTimeElement = document.querySelector("#time");
+  let currentDate = new Date();
+  let iconElement = document.querySelector("#icon");
+
   cityElement.innerHTML = response.data.city;
+  currentDayElement.innerHTML = formatDay(currentDate);
+  currentTimeElement.innerHTML = formatTime(currentDate);
+  descriptionElement.innerHTML = titleCase(response.data.condition.description);
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML =
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+
+function formatTime(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function formatDay(date) {
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let formattedDay = days[day];
+  return `${formattedDay}`;
 }
 
 function searchCity(city) {
@@ -20,5 +64,11 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .replace(/(^|\s)\S/g, (firstLetter) => firstLetter.toUpperCase());
+}
 
 searchCity("Bath");
